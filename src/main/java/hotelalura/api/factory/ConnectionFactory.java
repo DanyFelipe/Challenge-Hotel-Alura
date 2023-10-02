9a01b0d3b8cbbb8eae03b3b5a044f5ea6e0bfc10
DanyFelipe <danyfelipe117@gmail.com>
2023-10-02 04:53:11 -0500

    Version funcional de la aplicacion

---
+++ src/main/java/hotelalura/api/factory/ConnectionFactory.java
@@ -0,0 +1,31 @@
+package hotelalura.api.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel-alura?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("danylhall117");
+		
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexion() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
